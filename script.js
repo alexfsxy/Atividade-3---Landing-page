cdbab95db5868b8264d753fce1bd4d2f7c5a0a59
Ferreira <98367479+alexfsxy@users.noreply.github.com>
2024-06-25 23:37:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//* script.js  *//
+
+document.getElementById('nome').value = 'Cadastrar'
+function popUp() {
+    alert("Cadastramento realizado com sucesso!");
+    let janela = window.open("http://127.0.0.1:5500/class.01/unit3/atividade3/index.html", "nova janela");
+}
